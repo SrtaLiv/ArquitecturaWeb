64eab4716d65d4666376555ea22398f3ff3da8d4
Olivia <anaoliviatodesco@gmail.com>
2024-10-28 16:37:33 -0300

    init microservices

---
+++ Integra4/microservicio_user/src/main/java/com/microservicio_user/microservicio_user/MicroservicioUserApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio_user.microservicio_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioUserApplication.class, args);
+	}
+
+}
