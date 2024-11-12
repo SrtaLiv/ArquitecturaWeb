75aa380478fe7087240c28230877b4372eeda2c2
tomasquinteros2 <tomas.quinteros2002@gmail.com>
2024-11-12 17:07:20 -0300

    microservicio viaje - csv´s

--- Integrador4/microservicio_user/src/main/java/com/microservicio_user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservicio_user.repository;
-
-import com.microservicio_user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- Integrador4/microservicio_viaje/src/main/java/com/microservicio_user/repository/IRepositoryViaje.java
+++
@@ -1,9 +0,0 @@
-package com.microservicio_user.repository;
-
-import com.microservicio_user.model.Viaje;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IRepositoryViaje extends JpaRepository<Viaje, Long> {
-}
